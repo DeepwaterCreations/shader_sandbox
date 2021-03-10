b10f570c3fb952a0c82a0d6f806608e8f52a0d33
Dylan Craine <dylan.craine@deepwatercreations.com>
2021-03-10 14:18:10 -0700

    Create and bind triangle data

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main(int argv, char* argc[]){
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
+
 	//Make a window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Magic Portal", NULL, NULL);
 	if(window == NULL){
@@ -21,6 +22,7 @@ int main(int argv, char* argc[]){
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
 
 	//Set up viewport
 	glViewport(0,0,800,600);
@@ -32,6 +34,27 @@ int main(int argv, char* argc[]){
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO); //Bind it so we can put the forthcoming triangle in it.
 
+	//A simple triangle
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+	unsigned int VBO; // A vertex buffer object id
+	glGenBuffers(1, &VBO);
+	//Send the data to our buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //Bind our buffer to GL_ARRAY_BUFFER.
+	//Copy the vertex data to the buffer currently bound to GL_ARRAY_BUFFER:
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//Specify the format of our vertex data
+	glVertexAttribPointer(0, //Location of starting attribute
+			3, //Size of a vertex attribute: 3 values for x, y, z
+			GL_FLOAT, //Type of the data
+		       	GL_FALSE, //Does the data need to be converted to floats?
+		       	3 * sizeof(float), //Stride: The space between consecutive vertex attributes
+		       	(void*)0); //Offset of where data begins in the buffer
+	glEnableVertexAttribArray(0);
+	// Here we can unbind our VAO and make + bind the next one if we have more objects.
 	//Render Loop
 	while(!glfwWindowShouldClose(window)){
 		processInput(window);
